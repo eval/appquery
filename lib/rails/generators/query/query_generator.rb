ffaf6767601aa5742122e7f67e72c6fe9b292d16
Gert Goet <gert@thinkcreate.dk>
2026-01-10 16:55:14 +0100

    Generate ApplicationQuery and FooQuery.rb

--- lib/rails/generators/query/query_generator.rb
+++ lib/rails/generators/query/query_generator.rb
@@ -3,18 +3,33 @@ module Rails
     class QueryGenerator < NamedBase
       source_root File.expand_path("templates", __dir__)
 
+      def create_application_query
+        return if File.exist?(application_query_path)
+
+        template "application_query.rb", application_query_path
+      end
+
+      def create_query_class
+        template "query.rb",
+          File.join(query_path, class_path, "#{file_name}_query.rb")
+      end
+
       def create_query_file
         template "query.sql",
-          File.join(AppQuery.configuration.query_path, class_path, "#{file_name}.sql")
-
-        # in_root do
-        #  if behavior == :invoke && !File.exist?(application_mailbox_file_name)
-        #    template "application_mailbox.rb", application_mailbox_file_name
-        #  end
-        # end
+          File.join(query_path, class_path, "#{file_name}.sql")
       end
 
       hook_for :test_framework
+
+      private
+
+      def query_path
+        AppQuery.configuration.query_path
+      end
+
+      def application_query_path
+        File.join(query_path, "application_query.rb")
+      end
     end
   end
 end
